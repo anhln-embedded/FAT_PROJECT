c6b2b77086bedbb7835643d14b420ef9be0462ec
Cong Binh <congbinh.cbn@gmail.com>
2024-10-20 21:28:45 +0700

    read FAT update

--- check_fat.h
+++ check_fat.h
@@ -5,11 +5,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define FAT12_ENTRY_SIZE 1.5         // Kích thước thực sự của mỗi entry là 1.5 byte
+#define FAT12_ENTRY_SIZE 1.5            // Kích thước thực sự của mỗi entry là 1.5 byte
 #define FAT12_TOTAL_BYTE_IN_FAT 9*512
 
+uint16_t readEntry(uint16_t addressEntry, FILE * file);
+
 void read_fat_table(FILE *file);
-
-
 
 #endif // CHECK_FAT
--- main.c
+++ main.c
@@ -3,18 +3,46 @@
 #include <stdlib.h>
 #include "check_fat.h"
 
+uint16_t temp = 0x200 + 0xfe;
 
 int main()
 {
-
     FILE *file = fopen("floppy.img", "rb");
     if (file == NULL) {
-        perror("Error opening file");
+        printf("Error opening file");
         return EXIT_FAILURE;
     }
 
-    read_fat_table(file);
 
-    fclose("floppy.img");
+
+    do
+    {
+        temp = readEntry(temp, file);
+        printf("%03x \n", temp);
+
+        // dk
+        if(temp - 0x200 == 0x000)
+        {
+            printf("cluster's empty");
+            break;
+        }
+        else if((temp - 0x200) <= 0xFFF && (temp - 0x200) >= 0xFF8)
+        {
+            printf("EOF");
+            break;
+        }
+        else if((temp - 0x200) <= 0xFF6 && (temp - 0x200) >= 0xFF0)
+        {
+            printf("Reserved Values");
+            break;
+        }
+        else if (temp - 0x200 == 0xFF7)
+        {
+            printf("cluster error");
+            break;
+        }
+    }while( temp != 0xFFF);
+
+    fclose(file);
     return EXIT_SUCCESS;
 }
